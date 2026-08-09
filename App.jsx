784f15034b01a694c2482a657ae9db6c0da18a68
Ankit Mani Pandey <ankitpandey78600@gmail.com>
2026-08-09 13:51:42 +0530

    changed the project name to MovixPlus

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>MOVIX</Text>
+      <Text style={styles.title}>MOVIX+</Text>
       <Text style={styles.subtitle}>Stream Unlimited Movies & Trailers</Text>
       <StatusBar style="light" />
     </View>
