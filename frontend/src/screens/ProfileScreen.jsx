3e702f12c6bcf5e5ee12e2301dd96bd1424eecb8
Ankit Mani Pandey <ankitpandey78600@gmail.com>
2026-08-09 14:58:05 +0530

    added more screens in the frontend

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const LoginScreen = () => {
+const ProfileScreen = () => {
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <Text>ProfileScreen</Text>
     </View>
   )
 }
 
-export default LoginScreen
+export default ProfileScreen
 
 const styles = StyleSheet.create({})
